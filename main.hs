de8da1768cd931dca0effca88abe5b911f56fd0c
Xuyuan Pang <xuyuanp@gmail.com>
2015-11-02 12:10:18 +0800

    Beginnings of an evaluator: Primitives

--- main.hs
+++ main.hs
@@ -77,12 +77,16 @@ parseExpr = parseAtom
             _ <- char ')'
             return x
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-                     Left err -> "No match: " ++ show err
-                     Right val -> "Found " ++ show val
+                     Left err -> String $ "No match: " ++ show err
+                     Right val -> val
+
+eval :: LispVal -> LispVal
+eval val@(String _)             = val
+eval val@(Number _)             = val
+eval val@(Bool _)               = val
+eval (List [Atom "quote", val]) = val
 
 main :: IO ()
-main = do
-        (expr:_) <- getArgs
-        putStrLn $ readExpr expr
+main = getArgs >>= print . eval . readExpr . head
